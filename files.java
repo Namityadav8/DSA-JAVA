a7b95801432be02d24710613ed8fc33d32db4756
Namit <namityadav008@gmail.com>
2024-06-13 03:54:24 +0530

    made some changes in the code

--- files.java
+++ files.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.util.*;
 public class files {
     public static void main(String[] args) {
-        
+        File a = new File("abc.txt");
+        try{
+            a.createNewFile();
+        }catch(Exception e){}
+        finally{System.out.println("FILE created.");}
+
+        // now writing in this file 
+        try{
+        FileWriter fw = new FileWriter(a);
+        fw.write("Hi this is namit yadav");
+        fw.close();
+        }catch(Exception e){}
+        finally{System.out.println("Written something in the file ");}
     }
 }
