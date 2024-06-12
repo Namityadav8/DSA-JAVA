ce2c9953b0ed9cbdc73558ebb40921fc992ee84d
Namit <namityadav008@gmail.com>
2024-06-13 03:46:35 +0530

    made some changes in it

--- Basics.java
+++ Basics.java
@@ -1,18 +1,32 @@
 import java.util.*;
-interface a{
-    public void fun(String a);
+// interface a{
+//     public void fun(String a);
+// }
+class a{
+    public void fun(){
+        System.out.println("Inside the outer class");
+    }
+    class b{
+        public void fun(){
+            System.out.println("Inside the nested class");
+        }
+    }
 }
-
 
 public class Basics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your string");
-        String b = sc.nextLine();
-        a aa = (String a)->{
-            System.out.println("HELLO MF "+a);
-        };
-        aa.fun(b);
+        // System.out.println("Enter your string");
+        // String b = sc.nextLine();
+        // a aa = (String a)->{
+        //     System.out.println("HELLO MF "+a);
+        // };
+        // aa.fun(b);
+        a obj = new a();
+        a.b obj1 = obj.new b();
+        obj.fun();
+        obj1.fun();
+        
     }
 }
