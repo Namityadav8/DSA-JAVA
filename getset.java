d0cd935d4ebc29665bcf2694ab27a43af2e5be86
Namit <namityadav008@gmail.com>
2024-06-16 13:46:17 +0530

    made some changes

--- getset.java
+++ getset.java
@@ -1,18 +1,11 @@
-class a {
-    private int a ;
-    public void getNum(){
-       System.out.println("The value chosen is "+a);
-    }
-
-    public void setNum(int a){     
-        this.a = a;
-        
-    }
-}
+import java.util.*;
 public class getset {
     public static void main(String[] args) {
-        a  aa = new a();
-        aa.setNum(34);
-        aa.getNum();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        while(a>10){
+            a=a/10;
+        }
+        System.out.println(a);
     }
 }
