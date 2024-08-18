5dee4f5ed82ab280e2069e3a2d7b38d91c8849cd
Namit <namityadav008@gmail.com>
2024-08-19 00:01:33 +0530

    made some changes

--- Array/MaxArray.java
+++ Array/MaxArray.java
@@ -1,18 +1,27 @@
 package Array;
 
+import java.util.Arrays;
+
 public class MaxArray {
 
     public static void main(String[] args) {
         int a[] = {1,2,3,4,5,6};
-        int x=-1;
-        int ans=10;
+        int b[];
+        // cloning the elements of first array into another array.
+        // b=a.clone();
+        b=Arrays.copyOf(a, 3); // another way of cloning array but upto a particular length 
+        // int x=-1;
+        // int ans=10;
         
-        for(int i=0;i<a.length;i++){
-            if(a[i]==ans){
-                x=i;
-            }
+        // for(int i=0;i<a.length;i++){
+        //     if(a[i]==ans){
+        //         x=i;
+        //     }
+        // }
+        // System.out.println("Value found at index "+x);
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]);
         }
-        System.out.println("Value found at index "+x);
           
     }
 }
