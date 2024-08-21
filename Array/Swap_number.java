ac0729c5bc727aa66c84ad1e238c2956db92e9f7
Namit <namityadav008@gmail.com>
2024-08-21 10:44:27 +0530

    swapping numbers

---
+++ Array/Swap_number.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Swap_number {
+    public static void main(String[] args) {
+        int a =3;
+        int b = 9;
+        int temp;
+        temp = a;
+        a=b;
+        b=temp;
+        System.out.println(a+" "+b);
+    }
+}
