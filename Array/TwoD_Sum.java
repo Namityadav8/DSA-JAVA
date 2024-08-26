e9bb783886499b5d41a90000fadc2402aac3840d
Namit <namityadav008@gmail.com>
2024-08-26 18:39:55 +0530

    2D array

---
+++ Array/TwoD_Sum.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class TwoD_Sum {
+    public static void main(String[] args) {
+        int a[][]=new int[3][3];
+        int b[][]=new int[3][3];
+
+        int sum[][]=new int[3][3];
+
+        for(int i=0;i<sum.length;i++){
+            for(int j=0;j<sum[i].length;j++){
+                sum[i][j]=a[i][j] + b[i][j];
+            }
+        }
+
+
+        //almost complete just have to give values to a and b and its done 
+    }
+}
