263e1668a7b19e437d3abc8127dd0c968d34c2e1
Namit <namityadav008@gmail.com>
2024-08-29 22:31:18 +0530

    corrected

---
+++ Array/RotateMatrix.java
@@ -0,0 +1,58 @@
+// public class RotateMatrix {
+    
+//     // Method to transpose the matrix
+//     public static void transposeMatrix(int[][] matrix) {
+//         int n = matrix.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[j][i];
+//                 matrix[j][i] = temp;
+//             }
+//         }
+//     }
+
+//     // Method to reverse each row of the matrix
+//     public static void reverseRows(int[][] matrix) {
+//         int n = matrix.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n / 2; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[i][n - 1 - j];
+//                 matrix[i][n - 1 - j] = temp;
+//             }
+//         }
+//     }
+
+//     // Method to rotate the matrix 90 degrees clockwise
+//     public static void rotateMatrix(int[][] matrix) {
+//         transposeMatrix(matrix);
+//         reverseRows(matrix);
+//     }
+
+//     // Method to print the matrix
+//     public static void printMatrix(int[][] matrix) {
+//         for (int[] row : matrix) {
+//             for (int value : row) {
+//                 System.out.print(value + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int[][] matrix = {
+//             {1, 2, 3},
+//             {4, 5, 6},
+//             {7, 8, 9}
+//         };
+
+//         System.out.println("Original Matrix:");
+//         printMatrix(matrix);
+
+//         rotateMatrix(matrix);
+
+//         System.out.println("Matrix after 90-degree rotation:");
+//         printMatrix(matrix);
+//     }
+// }
