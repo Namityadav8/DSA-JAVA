05bc33c91076104cd086db44c6cc810b7e10cb5a
Namit <namityadav008@gmail.com>
2024-08-29 23:44:40 +0530

    recursion in deep

--- Recursion/Basic.java
+++ Recursion/Basic.java
@@ -1,9 +1,11 @@
 package Recursion;
 
 public class Basic {
+    // this function is for printing from 1 to n 
     static void printrun(int n){
         if(n>0){
-            printrun(n-1);
+            printrun(n-1); // here we have seen that we are calling the function again and it wont be going 
+            // to printing line before executing the function or returning to the main function
             System.out.println(n);
         }
     }
