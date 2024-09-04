0873dc7eabc9586a8e711ab05c789b6f9f1496c6
Namit <namityadav008@gmail.com>
2024-09-04 21:03:08 +0530

    sorting done

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -14,8 +14,10 @@ public class BubbleSort {
                 }
             }
         }
-        return a;
-    }
+        
+        return a; 
+}
+
     public static void main(String[] args) {
         
         int a[] = {3,4,2,5,1};
