fb47c82aa0f6eb27d768a55c0aed7c8fa53c9aeb
Namit <namityadav008@gmail.com>
2024-09-04 18:17:53 +0530

    selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,7 +1,32 @@
 package Sorting;
 
 public class SelectionSort {
+
+    
+
+    static int[] sorting(int a[]){
+        int n = a.length;
+
+        for(int i=0;i<n-1;i++){
+            int minindex=i;
+
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[minindex]){
+                    minindex=j;
+                }
+            }
+            int temp=a[i];
+            a[i]=a[minindex];
+            a[minindex]=temp;
+           
+        }
+        return a;
+    }
     public static void main(String[] args) {
-        
+        int a[]={4,2,14,1,34,6,9};
+        int b[]=sorting(a);
+        for(int i=0;i<b.length-1;i++){
+            System.out.println(b[i]);
+        }
     }
 }
