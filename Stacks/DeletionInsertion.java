c41a4f08988e5697bdeaf8df8c55e1863a829710
Namit <namityadav008@gmail.com>
2025-02-10 16:28:47 +0530

    recursive display

--- Stacks/DeletionInsertion.java
+++ Stacks/DeletionInsertion.java
@@ -1,7 +1,25 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class DeletionInsertion {
     public static void main(String[] args) {
-        
+        Stack<Integer> st = new Stack<>();
+        st.push(34);
+        st.push(2);
+        st.push(3);
+        st.push(99);
+        st.push(60);
+        //lets delete the bottom first - DownToEarth Person
+        System.out.println(st);
+        Stack<Integer> temp = new Stack<>();
+        while(st.size()>1){
+            temp.push(st.pop());
+        }
+        st.pop();
+        while(temp.size()>0){
+            st.push(temp.pop());
+        }
+        System.out.println(st);
     }
 }
---
+++ Stacks/RecursivelyDisplay.java
@@ -0,0 +1,19 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class RecursivelyDisplay {
+    public static void DisplayRec(Stack<Integer> farzIntegers){
+
+
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(99);
+        st.push(47);
+        st.push(82);
+        st.push(63);
+        st.push(420);
+        DisplayRec(st);
+    }
+}
